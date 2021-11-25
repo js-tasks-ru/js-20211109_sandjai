df45668293137e6c66212d802b6ead421e5117fc
Grigory Semakin <gsemakin@yahoo.com>
2021-11-25 22:41:27 +0300

    Task with Notification message resolved

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,59 @@
 export default class NotificationMessage {
 
+    static isShown = false;  
+
+    constructor(
+        message = '', {
+        duration = 0,
+        type = ''
+      } = {}) {
+
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        
+        this.render();
+    }    
+    
+    get template () {
+        return `<div class="notification ${this.type}" style="--value:${this.duration/1000}s">
+                    <div class="timer"></div>
+                    <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                    </div>
+                </div>`
+    }
+
+    render() {
+        const el = document.createElement('div');
+        el.innerHTML = this.template;
+        //this.element = element;
+        this.element = el.firstChild;
+    }
+
+    show(wrapper = document.createElement('div')) {        
+            if (!NotificationMessage.isShown) {
+                
+                wrapper.append(this.element);
+                document.body.append(wrapper);
+                NotificationMessage.isShown = true;
+                setTimeout(() => {this.destroy()}, `${this.duration}`);
+            }
+        }
+
+    remove() {
+        if (this.element) {
+        this.element.remove();
+        }
+    }
+
+    destroy() {       
+        this.remove();
+        this.element = null;
+        NotificationMessage.isShown = false;     
+    }
+
 }
